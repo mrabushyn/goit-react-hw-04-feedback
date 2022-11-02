81cc236f27dd3078fa45cc908b55674ae68a278e
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-11-02 18:57:03 +0200

    refactor on huks

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import css from './Feedback.module.css';
 import PropTypes from 'prop-types';
 
@@ -7,95 +7,74 @@ import { FeedbackOptions } from './FeedbackOptions';
 import { Section } from './Section';
 import { Notification } from './Notification';
 
-export class Feedback extends Component {
-    state = {
-      valueForGood: this.props.initialValue,
-      valueForNeutral: this.props.initialValue,
-      valueForBad: this.props.initialValue,
-      valueForTotal: this.props.initialValue,
-      valueForPercentage: this.props.initialValuePercentage,
-    };
 
-  handleIncrementForGood() {
-    this.setState((state, props) => ({
-      valueForGood: state.valueForGood + props.step,
-    }));
-    this.countTotalFeedback();
-    this.countPositiveFeedbackPercentage();
-  }
+export function Feedback ({initialValue, step, initialValuePercentage}) {
+  const [valueForGood, setValueForGood] = useState(initialValue);
+  const [valueForNeutral, setValueForNeutral] = useState(initialValue);
+  const [valueForBad, setValueForBad] = useState(initialValue);
+  const [valueForTotal, setValueForTotal] = useState(initialValue);
+  const [valueForPercentage, setValueForPercentage] = useState(
+    initialValuePercentage
+  );
 
-  handleIncrementForNeutral() {
-    this.setState((state, props) => ({
-      valueForNeutral: state.valueForNeutral + props.step,
-    }));
-    this.countTotalFeedback();
-    this.countPositiveFeedbackPercentage();
-  }
+  const handleIncrementForGood = () => {
+    setValueForGood(valueForGood + step);
+    countTotalFeedback();
+    countPositiveFeedbackPercentage();
+  };
 
-  handleIncrementForBad() {
-    this.setState((state, props) => ({
-      valueForBad: state.valueForBad + props.step,
-    }));
-    this.countTotalFeedback();
-    this.countPositiveFeedbackPercentage();
-  }
+  const handleIncrementForNeutral = () => {
+    setValueForNeutral(valueForNeutral + step);
+    countTotalFeedback();
+    countPositiveFeedbackPercentage();
+  };
 
-  countTotalFeedback() {
-    this.setState(state => ({
-      valueForTotal:
-        state.valueForGood + state.valueForNeutral + state.valueForBad,
-    }));
-  }
+  const handleIncrementForBad = () => {
+    setValueForBad(valueForBad + step);
+    countTotalFeedback();
+    countPositiveFeedbackPercentage();
+  };
 
-  countPositiveFeedbackPercentage() {
-    this.setState(state => ({
-      valueForPercentage: (
-        (state.valueForGood / state.valueForTotal) *
-        100
-      ).toFixed(0),
-    }));
-  }
+  const  countTotalFeedback = () => {
+setValueForTotal(valueForGood + valueForNeutral + valueForBad)
+    }
 
-  render() {
+  const countPositiveFeedbackPercentage = () => {
+setValueForPercentage ( ((valueForGood / valueForTotal) *  100).toFixed(0) )
+    }
 
-    this.handleIncrementForGood = this.handleIncrementForGood.bind(this);
-    this.handleIncrementForNeutral = this.handleIncrementForNeutral.bind(this);
-    this.handleIncrementForBad = this.handleIncrementForBad.bind(this);
+  return (
+    <div className={css.mainContainer}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          good={handleIncrementForGood}
+          neutral={handleIncrementForNeutral}
+          bad={handleIncrementForBad}
+        />
+      </Section>
 
-
-    return (
-      <div className={css.mainContainer}>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            good={this.handleIncrementForGood}
-            neutral={this.handleIncrementForNeutral}
-            bad={this.handleIncrementForBad}
+      <Section title="Statistics">
+        {valueForTotal === 0 ? (
+          <Notification message="There is no feedback"></Notification>
+        ) : (
+          <Statistics
+            good={valueForGood}
+            neutral={valueForNeutral}
+            bad={valueForBad}
+            total={valueForTotal}
+            positivePercentage={valueForPercentage}
           />
-        </Section>
-
-        <Section title="Statistics">
-          {this.state.valueForTotal === 0 ? (
-            <Notification message="There is no feedback"></Notification>
-          ) : (
-            <Statistics
-              good={this.state.valueForGood}
-              neutral={this.state.valueForNeutral}
-              bad={this.state.valueForBad}
-              total={this.state.valueForTotal}
-              positivePercentage={this.state.valueForPercentage}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        )}
+      </Section>
+    </div>
+  );
 }
 
 
 Feedback.propTypes = {
-  step: PropTypes.number.isRequired,
-  initialValue: PropTypes.number.isRequired,
-  initialValuePercentage: PropTypes.number.isRequired,
+  step: PropTypes.number,
+  initialValue: PropTypes.number,
+  initialValuePercentage: PropTypes.number,
 };
 
 
